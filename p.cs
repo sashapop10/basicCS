f40aeca8c41e37b0ae1e1b7a53aca8cf87daa8a1
Alexander Ivanov <sashapop10@yandex.ru>
2022-12-17 23:08:06 +0300

    first commit

---
+++ p.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+
+namespace hackingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            
+            while (true)
+            {
+
+                Random rand = new Random();
+
+                   int num_letters = rand.Next(100, letters.Length - 1 + 100);
+                for (int j = 0; j <= num_letters; j++)
+                    {
+                    
+                        int latter_num = rand.Next(0, letters.Length - 1);
+
+                    int Timer = rand.Next(0, 1);
+                        System.Threading.Thread.Sleep(Timer);
+                        if (j != num_letters)
+                        {
+                            Console.Write(letters[latter_num]);
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                
+                
+                
+                
+                
+                
+            }
+
+        }
+        
+
+    }
+}
---
+++ p2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HackingProgram2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz-+/|?.><*,#$@%^&()_=:;`~''".ToCharArray();
+            bool Flag = true;
+            for (int v = 0; Flag && v < letters.Length; v++) { 
+            int m = letters.Length, n = v;
+            for (int i = 0; Flag && i < Math.Pow(m, n); i++)
+            {
+                string s = "";
+                int ii = i;
+                for (int j = 0; j < n; j++)
+                {
+                    s = letters[ii % m] + s;
+                    ii /= m;
+                }
+                    
+                    
+                    Console.WriteLine(s);
+                    if(s == password)
+                    {
+                        Flag = false;
+                        Console.WriteLine($"password was detected:{s}");
+                    }
+            }
+            }
+            Console.ReadKey();
+        }
+    }
+
+    }
+
